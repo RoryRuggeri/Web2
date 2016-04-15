ddc380a9500c16e1e741504b042779a0ac7447ec
RoryRuggeri <rossana.ruggeriy@gmail.com>
2016-04-15 11:46:43 -0400

    Semana5
    Semana5

---
+++ Semana5/js/script.js
@@ -0,0 +1,82 @@
+$("#demo01").animatedModal({
+                modalTarget:'elemento1',
+                animatedIn:'zoomIn',
+                animatedOut:'zoomOut',
+                color:'#3498db',
+                // Callbacks
+                beforeOpen: function() {
+                    console.log("The animation was called");
+                },           
+                afterOpen: function() {
+                    console.log("The animation is completed");
+                }, 
+                beforeClose: function() {
+                    console.log("The animation was called");
+                }, 
+                afterClose: function() {
+                    console.log("The animation is completed");
+                }
+            });
+
+
+$("#demo02").animatedModal({
+                modalTarget:'elemento2',
+                animatedIn:'zoomIn',
+                animatedOut:'zoomOut',
+                color:'#3498db',
+                // Callbacks
+                beforeOpen: function() {
+                    console.log("The animation was called");
+                },           
+                afterOpen: function() {
+                    console.log("The animation is completed");
+                }, 
+                beforeClose: function() {
+                    console.log("The animation was called");
+                }, 
+                afterClose: function() {
+                    console.log("The animation is completed");
+                }
+            });
+
+
+$("#demo03").animatedModal({
+                modalTarget:'elemento3',
+                animatedIn:'zoomIn',
+                animatedOut:'zoomOut',
+                color:'#3498db',
+                // Callbacks
+                beforeOpen: function() {
+                    console.log("The animation was called");
+                },           
+                afterOpen: function() {
+                    console.log("The animation is completed");
+                }, 
+                beforeClose: function() {
+                    console.log("The animation was called");
+                }, 
+                afterClose: function() {
+                    console.log("The animation is completed");
+                }
+            });
+
+
+$("#demo04").animatedModal({
+                modalTarget:'elemento4',
+                animatedIn:'zoomIn',
+                animatedOut:'zoomOut',
+                color:'#3498db',
+                // Callbacks
+                beforeOpen: function() {
+                    console.log("The animation was called");
+                },           
+                afterOpen: function() {
+                    console.log("The animation is completed");
+                }, 
+                beforeClose: function() {
+                    console.log("The animation was called");
+                }, 
+                afterClose: function() {
+                    console.log("The animation is completed");
+                }
+            });
